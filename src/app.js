06e1a6cabe2055a3e29865291df3130e4f81db13
Stephen Weiss <stephencweiss@gmail.com>
2019-02-15 16:15:52 -0600

    Added more eslint configurations and separated out Pet and app
    Removed script tags from index.html

--- src/app.js
+++ src/app.js
@@ -1,44 +1,21 @@
-const React = require('React');
+import React from "react";
+import { render } from "react-dom";
+import { Pet } from "./Pet";
 
-// Create a class, App
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
-
-// class component
 class App extends React.Component {
   handleTitleClick() {
     alert("You clicked the title!");
   }
   render() {
-    return React.createElement("div", {}, [
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Adopt Me!"
-      ),
-      React.createElement(Pet, {
-        name: "Finn",
-        animal: "Dog",
-        breed: "Mix"
-      }),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese"
-      }),
-      React.createElement(Pet, {
-        name: "Lucy",
-        animal: "Dog",
-        breed: "Labrador"
-      })
-    ]);
+    return (
+      <div>
+        <h1 onClick={this.handleTitleClick}>Adopt Me!</h1>
+        <Pet name="Finn" animal="Dog" breed="Mix" />
+        <Pet name="Luna" animal="Dog" breed="Havanese" />
+        <Pet name="Lucy" animal="Dog" breed="Labrador" />
+      </div>
+    );
   }
 }
 
-// Create an instance of my class
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
