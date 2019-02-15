117c14c3d983b2f90d3633979758bdfbab3b1cbe
Stephen Weiss <stephencweiss@gmail.com>
2019-02-15 14:19:40 -0600

    Refactor Pets to accept Props
    Signed-off-by: Stephen Weiss <stephencweiss@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
 // Create a class, App
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Finn"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Lab-Mix"),
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
   ])
 }
 
 const App = () => {
   return React.createElement("div", {},[
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, {
+      name: 'Finn',
+      animal:'Dog',
+      breed: 'Mix'
+    }),
+    React.createElement(Pet, {
+      name: 'Luna',
+      animal:'Dog',
+      breed: 'Havanese'
+    }),
+    React.createElement(Pet, {
+      name: 'Lucy',
+      animal:'Dog',
+      breed: 'Labrador'
+    }),
   ])
 }
 
