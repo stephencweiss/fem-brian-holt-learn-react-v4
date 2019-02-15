f52b2f3e7eb76a0eec20a7c5ba2827acfce606b8
Stephen Weiss <stephencweiss@gmail.com>
2019-02-15 17:17:26 -0600

    Refactored for state & lifecycle for pets
    Pets now dynamically renders for all pets retrieved on mount. These are then passed down to Pet to generate as many components as needed

--- src/Pet.js
+++ src/Pet.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 // Create a class, App
 
-const Pet = props => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
-  );
-};
+class Pet extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const { name, animal, breed, media, location } = this.props;
+    let photos = [];
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+    return (
+      <div className="pet">
+        <div className="image-container">
+          <img src={photos[0].value} alt={name} />
+        </div>
+        <div className="info">
+          {animal} - {name} - {breed} - {location}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Pet;
--- src/app.js
+++ src/app.js
@@ -1,18 +1,63 @@
 import React from "react";
 import { render } from "react-dom";
-import { Pet } from "./Pet";
+import pf from "petfinder-client";
+import Pet from "./Pet";
+
+const petFinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
-  handleTitleClick() {
-    alert("You clicked the title!");
+  constructor(props) {
+    super(props);
+    this.state = {
+      pets: []
+    };
+  }
+  componentDidMount() {
+    petFinder.pet
+      .find({ output: "full", location: "Chicago, IL", animal: "dog" })
+      .then(data => {
+        let pets;
+        // console.log(`The data is --> `, data);
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+        console.log(`pets is now--> `, pets);
+        this.setState({ pets });
+      });
   }
   render() {
     return (
       <div>
-        <h1 onClick={this.handleTitleClick}>Adopt Me!</h1>
-        <Pet name="Finn" animal="Dog" breed="Mix" />
-        <Pet name="Luna" animal="Dog" breed="Havanese" />
-        <Pet name="Lucy" animal="Dog" breed="Labrador" />
+        <h1>Adopt Me!</h1>
+        <div>
+          {this.state.pets.map(pet => {
+            let breed;
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(", ");
+            } else {
+              breed = pet.breeds.breed;
+            }
+            return (
+              <Pet
+                key={pet.id}
+                animal={pet.animal}
+                name={pet.name}
+                breed={breed}
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
