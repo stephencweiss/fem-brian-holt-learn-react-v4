d6ce55b2e5303ba5de63b77e1330fdc26c7dfe52
Stephen Weiss <stephencweiss@gmail.com>
2019-02-15 15:23:34 -0600

    Configuring eslint and prettier

--- src/app.js
+++ src/app.js
@@ -1,41 +1,44 @@
+const React = require('React');
+
 // Create a class, App
-const Pet = (props) => {
+const Pet = props => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
     React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ])
-}
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
 
 // class component
-class App extends React.Component{
+class App extends React.Component {
   handleTitleClick() {
-    alert('You clicked the title!');
+    alert("You clicked the title!");
   }
   render() {
-    return React.createElement("div", {},[
-      React.createElement("h1", { onClick: this.handleTitleClick}, "Adopt Me!"),
+    return React.createElement("div", {}, [
+      React.createElement(
+        "h1",
+        { onClick: this.handleTitleClick },
+        "Adopt Me!"
+      ),
       React.createElement(Pet, {
-        name: 'Finn',
-        animal:'Dog',
-        breed: 'Mix'
+        name: "Finn",
+        animal: "Dog",
+        breed: "Mix"
       }),
       React.createElement(Pet, {
-        name: 'Luna',
-        animal:'Dog',
-        breed: 'Havanese'
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese"
       }),
       React.createElement(Pet, {
-        name: 'Lucy',
-        animal:'Dog',
-        breed: 'Labrador'
-      }),
-    ])
+        name: "Lucy",
+        animal: "Dog",
+        breed: "Labrador"
+      })
+    ]);
   }
 }
 
 // Create an instance of my class
-ReactDOM.render(
-  React.createElement(App),
-  document.getElementById('root')
-)
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
